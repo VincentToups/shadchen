d7d6da94ebf3589aad81f5fd1877ba018edf1d84
Vincent Toups <vincent.toups@gmail.com>
2012-03-03 08:57:04 -0500

    updated exports

--- package.lisp
+++ package.lisp
@@ -2,7 +2,8 @@
 
 (defpackage #:shadchen
 ;;;  (:export "MATCH" "MATCH-LAMBDA" "DEFPATTERN")
-  (:export :match :match-lambda :defpattern)
+  (:export :match :match-lambda :defpattern
+		   :? :bq :list-rest :hash-table :struct :let1)
   (:use #:cl))
 
 
