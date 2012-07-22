8bc32e6bdeb43d401f38a5c28722e385e9779e55
Vincent Toups <vincent.toups@gmail.com>
2012-07-22 15:36:40 +0200

    added nickname

--- package.lisp
+++ package.lisp
@@ -1,6 +1,7 @@
 ;;;; package.lisp
 
 (defpackage #:shadchen
+  (:nicknames :s?)
   (:export :? :bq :p :list-rest :hash-table :struct :let1 
 		   :match :match-lambda :defpattern
 		   :match-let :match-let* :match-loop
