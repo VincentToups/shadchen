450dc48f33124ce5463e3a0a3492cc12696f3a16
Vincent Toups <vincent.toups@gmail.com>
2012-07-22 15:06:28 +0200

    exported _

--- package.lisp
+++ package.lisp
@@ -3,7 +3,8 @@
 (defpackage #:shadchen
   (:export :? :bq :p :list-rest :hash-table :struct :let1 
 		   :match :match-lambda :defpattern
-		   :match-let :match-let* :match-loop)
+		   :match-let :match-let* :match-loop
+		   :_)
   (:use #:cl))
 
 
