6ab63280cee229982ea6d93e86056c661cc50f8f
Vincent Toups <vincent.toups@gmail.com>
2013-02-27 08:21:57 -0500

    NORMAL exported defun-match/-

--- package.lisp
+++ package.lisp
@@ -3,7 +3,7 @@
 (defpackage #:shadchen
   (:nicknames :s?)
   (:export :? :bq :p :list-rest :hash-table :struct :let1 
-		   :must-match :!
+		   :must-match :! :defun-match :defun-match-
 		   :match :match-lambda :defpattern
 		   :match-let :match-let* :match-loop
 		   :tail :number :symbol :string :keyword
